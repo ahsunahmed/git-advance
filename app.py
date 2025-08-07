0eb69b695914b19f9d4decc4fcd4b5a7bfd9626e
Ahsun Ahmed Sun <ahsun.ahmed.cse@gmail.com>
2025-08-08 02:13:05 +0600

    Used Flake8 as a linter tool & Black as a formatter tool

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 class Student:
-    def __init__(self,id,name,student_class,section,shift):
+    def __init__(self, id, name, student_class, section, shift):
         self.id = id
         self.name = name
         self.student_class = student_class
@@ -9,6 +9,8 @@ class Student:
     def isPresent(self):
         print(f"{self.name} is present today!")
 
-student1 = Student(1,"Ahsun",10,1,'Morning')
+
+student1 = Student(1, "Ahsun", 10, 1, "Morning")
+
 
 student1.isPresent()
